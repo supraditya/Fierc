91ae6c83fb188c46e2ce8e59fbadeabc79d863d6
[supraditya] <[aditya300100@gmail.com]>
2020-05-18 22:43:32 +0530

    Fixed navbar and mobile navbar styling

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from '../stylesheets/MobileNav.module.css';
 import {Link} from 'react-router-dom';
-const MobileNav=()=>{
-    return(
-        <div className="container"> 
-         <ul className={styles.list}>
-            <div className={styles.nav}>
-                    <li><Link to="/"><span><i class="fa fa-home icon-large"></i></span></Link></li>
-                    <li><Link to="/profile"><span><i class="fa fa-user icon-large" ></i></span></Link></li>
-                    <li><Link to="/emergency"><span><i class="fa fa-exclamation-triangle icon-large" aria-hidden="true"></i></span></Link></li>
+class MobileNav extends Component{
+    render()
+    {
+        const spanstyle={
+            fontSize: '45px'
+        };
+        return(
+            <div className="MobileNav">
+                <div className="container"> 
+                <ul className={styles.list}>
+                    <div className={styles.nav}>
+                            <li><Link to="/"><span style={spanstyle}><i class="fa fa-home icon-large"></i></span></Link></li>
+                            <li><Link to="/profile"><span style={spanstyle}><i class="fa fa-user icon-large" ></i></span></Link></li>
+                            <li><Link to="/emergency"><span style={spanstyle}><i class="fa fa-exclamation-triangle icon-large" aria-hidden="true"></i></span></Link></li>
+                    </div>
+                    </ul>
+                </div>
             </div>
-            </ul>
-        </div>
-    )
+        )
+    }
 }
 export default MobileNav;
