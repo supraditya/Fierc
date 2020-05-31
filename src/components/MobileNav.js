abf6cd9595c939b467b5ad7a84b40fcaab980766
[supraditya] <[aditya300100@gmail.com]>
2020-05-31 23:23:14 +0530

    Partially solved globalised styling of font awesome icons, re-aligned mobile view elements

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -5,16 +5,17 @@ class MobileNav extends Component{
     render()
     {
         const spanstyle={
-            fontSize: '45px'
+            fontSize: '45px',
+            color: '#484848',
         };
         return(
             <div className="MobileNav">
                 <div className="container"> 
                 <ul className={styles.list}>
                     <div className={styles.nav}>
-                            <li><Link to="/"><span style={spanstyle}><i class="fa fa-home icon-large"></i></span></Link></li>
-                            <li><Link to="/profile"><span style={spanstyle}><i class="fa fa-user icon-large" ></i></span></Link></li>
-                            <li><Link to="/emergency"><span style={spanstyle}><i class="fa fa-exclamation-triangle icon-large" aria-hidden="true"></i></span></Link></li>
+                            <li><Link to="/"><span style={spanstyle}><i className="fa fa-home icon-large"></i></span></Link></li>
+                            <li><Link to="/profile"><span style={spanstyle}><i className="fa fa-user icon-large"></i></span></Link></li>
+                            <li><Link to="/emergency"><span style={spanstyle}><i className="fa fa-exclamation-triangle icon-large"></i></span></Link></li>
                     </div>
                     </ul>
                 </div>
